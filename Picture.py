c1ef30c0e4a94681b8028956720f1309f6f7a50c
Tattaboe <polinaohayo314@mail.ru>
2023-09-10 01:19:49 +0400

    Trying to create a folder

--- Picture.py
+++ Picture.py
@@ -1,1 +1,10 @@
-print ("hello")
+
+import os
+path = '/Users/polina/Documents/Python_Lab/Programm-lab '
+projectname = 'dataset*'
+folders = \
+[ '*dog*', 
+ '*cat*']
+
+fullPath = os.path.join(path,projectname)
+os.mkdir(fullPath)
