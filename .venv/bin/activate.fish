87d9c5fef011881fc87366f2ffcc159a9cddce04
Tattaboe <polinaohayo314@mail.ru>
2023-11-26 23:58:37 +0400

    creating a new venv

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/polina/Documents/Python_Lab/Programm-lab/.venv"
+set -gx VIRTUAL_ENV "/Users/polina/Documents/Programm-lab/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
