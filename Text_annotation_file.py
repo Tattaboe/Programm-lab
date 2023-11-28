4abb58e934475b95ef16d7d59071cc872a7dbbd2
Tattaboe <polinaohayo314@mail.ru>
2023-11-29 01:01:39 +0400

    end_commit

--- Text_annotation_file.py
+++ Text_annotation_file.py
@@ -4,6 +4,20 @@ from typing import List
 
 
 def get_full_paths(class_name: str) -> List[str]:
+    """
+    Возвращает список абсолютных путей для изображений
+
+    Данная функция возвращает список абсолютных путей для всех изображений определенного
+    класса, переданного в функцию
+    Parameters
+    ----------
+    class_name : str
+      Имя класса
+    Returns
+    -------
+    list
+    Список абсолютных путей к изображениям
+    """
     full_path = os.path.abspath('dataset')
     class_path = os.path.join(full_path, class_name)
     image_names = os.listdir(class_path)
@@ -12,23 +26,22 @@ def get_full_paths(class_name: str) -> List[str]:
     return image_full_paths
 
 
-
 def main() -> None:
- 
- dataset_path = "dataset"
- classes = ["cats", "dogs"]
+    dataset_path = "dataset"
+    classes = ["cats", "dogs"]
 
- with open("annotation_2.csv", "w", newline="") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(["full_path", "relative_path", "class"])
+    with open("annotation.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(["full_path", "relative_path", "class"])
 
-    for class_name in classes:
-        class_path = os.path.join(dataset_path, class_name)
+        for class_name in classes:
+            class_path = os.path.join(dataset_path, class_name)
+            full_paths = get_full_paths(class_name)
 
-        for file_name in os.listdir(class_path):
-           
-            relative_path = os.path.join(class_name, file_name)
-            writer.writerow([full_path, relative_path, class_name])
+            for full_path in full_paths:
+                relative_path = os.path.relpath(full_path, dataset_path)
+                writer.writerow([full_path, relative_path, class_name])
+
 
 if __name__ == "__main__":
     main()
